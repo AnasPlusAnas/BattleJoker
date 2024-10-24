fcddc99d0b4b2d3023b6cbff51c8833c5f755958
ANAS-T14S <anas.anas@pccw.com>
2024-10-24 15:28:26 +0800

    add ip and port user input

--- src/GetNameDialog.java
+++ src/GetNameDialog.java
@@ -2,23 +2,34 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class GetNameDialog {
     @FXML
-    TextField nameField;
+    TextField nameField, ipField, portField;
+
+    @FXML
+    Label errorLabel;
 
     @FXML
     Button goButton;
 
+    @FXML
+    GridPane getNameDialogPane;
+
     Stage stage;
     String playername;
+    String ip;
+    String port;
 
     public GetNameDialog() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("getNameUI.fxml"));
@@ -32,6 +43,8 @@ public class GetNameDialog {
         stage.setMinWidth(scene.getWidth());
         stage.setMinHeight(scene.getHeight());
 
+        getNameDialogPane.setPadding(new Insets(10, 10, 10, 10));
+
         goButton.setOnMouseClicked(this::OnButtonClick);
 
         stage.showAndWait();
@@ -40,8 +53,32 @@ public class GetNameDialog {
     @FXML
     void OnButtonClick(Event event) {
         playername = nameField.getText().trim();
-        if (playername.length() > 0)
+        ip = ipField.getText().trim();
+        port = portField.getText().trim();
+
+        if (!playername.isEmpty() && !ip.isEmpty() && !port.isEmpty()) {
+            // add regex to check if ip is a valid ipv4 address
+            if (!ip.matches("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$")) {
+                errorLabel.setText("Invalid IP address");
+                return;
+            }
+
+            // Validate if port is an integer
+            try {
+                int portInt = Integer.parseInt(port);
+                // Optionally check if the port is within a valid range (e.g., 1-65535)
+                if (portInt < 1 || portInt > 65535) {
+                    errorLabel.setText("Port must be between 1 and 65535.");
+                    return;
+                }
+            } catch (NumberFormatException e) {
+                errorLabel.setText("Invalid port number.");
+                return;
+            }
+
             stage.close();
+        }
+        errorLabel.setText("One or more input(s) are empty.");
     }
 
     public String getPlayername() {
